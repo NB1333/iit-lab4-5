55007673bef81c814b45608c2e35d1e39c9bd1b5
Nikita Bykov <bykov.nikita@lll.kpi.ua>
2024-03-26 06:33:26 +0200

    EDITED: - actions config

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-var foo = true, bar = false;
 
 export const metadata = {
   title: "Create Next App",
