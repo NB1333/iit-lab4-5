43a978a524cc45a8d1aac48c7714953f29515dd2
Nikita Bykov <bykov.nikita@lll.kpi.ua>
2024-04-15 23:07:48 +0300

    EDITED:  - main.tf

--- main.tf
+++ main.tf
@@ -4,36 +4,7 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-080e1f13689e07408"
-  instance_type = var.instance_type
-  key_name      = "keyforlab6"
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update
-              sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-              sudo apt-get update
-              sudo apt-get install -y docker-ce
-              sudo usermod -aG docker ubuntu
-              sudo systemctl start docker
-              sudo systemctl enable docker
-              docker pull nb1333/lab4-5:latest
-              docker run -d -p 80:3000 nb1333/lab4-5:latest
-              docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --interval 60
-              EOF
-
-  tags = {
-    Name = "lab6Instance"
-  }
-
-  vpc_security_group_ids = [data.aws_security_group.allow_web.id]
-}
-
 resource "aws_security_group" "allow_web" {
-  count       = "${length(data.aws_security_groups.allow_web.ids) == 0 ? 1 : 0}"
   name        = "allow_web_traffic_unique"
   description = "Allow web inbound traffic"
   
@@ -65,4 +36,32 @@ resource "aws_security_group" "allow_web" {
 
 data "aws_security_group" "allow_web" {
   name = "allow_web_traffic_unique"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-080e1f13689e07408"
+  instance_type = var.instance_type
+  key_name      = "keyforlab6"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+              sudo apt-get update
+              sudo apt-get install -y docker-ce
+              sudo usermod -aG docker ubuntu
+              sudo systemctl start docker
+              sudo systemctl enable docker
+              docker pull nb1333/lab4-5:latest
+              docker run -d -p 80:3000 nb1333/lab4-5:latest
+              docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --interval 60
+              EOF
+
+  tags = {
+    Name = "lab6Instance"
+  }
+
+  vpc_security_group_ids = [aws_security_group.allow_web.id]
 }
